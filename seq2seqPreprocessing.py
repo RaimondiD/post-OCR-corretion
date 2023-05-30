fb24bbc1d8c855700aa17d8a60cb9e0f93bea04d
Davide Raimondi <daduraimondi@gmail.com>
2023-05-30 10:24:45 +0200

    first try to build seq2seq with t5 tokenizer

--- seq2seqPreprocessing.py
+++ seq2seqPreprocessing.py
@@ -4,6 +4,9 @@ import regex as re
 from typing import Callable
 import more_itertools
 import itertools
+import transformers
+from torch.utils.data import DataLoader, TensorDataset
+import torch
 
 DATASET_DIRCECTORY = Path("dataset")
 TEST_INPUT_FILE = Path(DATASET_DIRCECTORY,"train_output.csv") 
@@ -78,7 +81,10 @@ def split_longer_sequences(long_sequences:list[str], too_long :Callable[[str],bo
 def split_in_middle_space(long_char_sequence : str):
     indexes_of_space = more_itertools.locate(long_char_sequence, lambda char : char == DEFAULT_SEPARATOR_SYMBOL) #return the list of index when a space is found
     worst_space_metrics = lambda index : abs(index - (len(long_char_sequence)/2))  #i want to split the sequence in the space that is more central
-    best_index = min(indexes_of_space,key = worst_space_metrics)
+    try:
+        best_index = min(indexes_of_space,key = worst_space_metrics)
+    except:
+        return []
     return (long_char_sequence[:best_index],long_char_sequence[best_index+1:])
 
 
@@ -91,10 +97,69 @@ def make_input_dataset(list_of_sequences : list[str]):
         remove_separator_symbols_function = substitute_symbol_func_factory(f" {DEFAULT_SEPARATOR_SYMBOL} ")
         return list(map(remove_separator_symbols_function,list_of_sequences))
 
+def encode_for_T5(input_dataset,output_dataset):
+    tokenizer_t5 = transformers.T5Tokenizer.from_pretrained('t5-small')
+    dict_of_tokenized_dataset = tokenizer_t5(input_dataset,text_target = output_dataset, 
+                                             max_length = MAX_SEQUENCE_LEN, 
+                                            truncation = True, padding = True, 
+                                            return_tensors = "pt")
+    return dict_of_tokenized_dataset['input_ids'], dict_of_tokenized_dataset['attention_mask'], dict_of_tokenized_dataset['labels']
+    
+    
 
-dataset_sample = load_dataset()[:2]
+class Seq2SeqDataset(torch.utils.data.Dataset):
+    def __init__(self, input_texts, target_texts, tokenizer):
+        self.input_texts = input_texts
+        self.target_texts = target_texts
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.input_texts)
+
+    def __getitem__(self, idx):
+        input_text = self.input_texts[idx]
+        target_text = self.target_texts[idx]
+        tokenized = self.tokenizer(input_text,text_target = target_text, 
+                                             max_length = MAX_SEQUENCE_LEN, 
+                                            truncation = True, padding = True, 
+                                            return_tensors = "pt")
+        print(tokenized["input_ids"].size(),tokenized[0]['labels'].size)
+        return tokenized["input_ids"], tokenized['attention_mask'], tokenized["labels"]
+
+from transformers import DataCollatorForSeq2Seq
+
+class CustomDataCollator(DataCollatorForSeq2Seq):
+    def __call__(self, features):
+        input_ids = [f[0] for f in features]
+        labels = [f[1] for f in features]
+
+        batch = self.tokenizer.pad({"input_ids": input_ids, "labels": labels}, return_tensors="pt")
+        return batch
+
+
+dataset_sample = load_dataset()[:20]
 cleaned_dataset_sample = clean_dataset(dataset_sample)
 sequences_of_char_sample = separate_chars_with_spaces(cleaned_dataset_sample)
 good_length_sequences_of_char_sample = shorten_sequences_to_target_lenght(sequences_of_char_sample)
 output_dataset_sample = make_output_dataset(good_length_sequences_of_char_sample)
 input_dataset_sample = make_input_dataset(good_length_sequences_of_char_sample)
+tokenizer_t5 = transformers.T5Tokenizer.from_pretrained('t5-small')
+batch_size = 8
+dataset = Seq2SeqDataset(input_dataset_sample,output_dataset_sample, tokenizer_t5)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+model = transformers.T5ForConditionalGeneration.from_pretrained('t5-small')
+data_collator = CustomDataCollator(tokenizer_t5)
+training_args = transformers.TrainingArguments(
+    output_dir='./results',
+    num_train_epochs=3,
+    per_device_train_batch_size=16,
+    logging_dir='./logs',
+)
+
+trainer = transformers.Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,  
+    data_collator= data_collator
+)
+trainer.train()
